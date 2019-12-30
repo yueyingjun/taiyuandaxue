24a77cce87602d697d6db8ff1e39580366896a1a
yueyingjun <yueyingjun@qq.com>
2019-12-30 17:55:47 +0800

    gangqin

---
+++ gangqin.js
@@ -0,0 +1,60 @@
+window.onload=function (ev) {
+
+    /*
+    * 49   1
+    * 50  2
+    *
+    *
+    *
+    * 56    8
+    *
+    * */
+
+    var hash={
+        49:{
+            div:0,sound:400+400*(1/8)
+        },
+        50:{
+            div:1,sound:400+400*(2/8)
+        },
+        51:{
+            div:2,sound:400+400*(3/8)
+        },
+        52:{
+            div:3,sound:400+400*(4/8)
+        },
+        53:{
+            div:4,sound:400+400*(5/8)
+        },
+        54:{
+            div:5,sound:400+400*(6/8)
+        },
+        55:{
+            div:6,sound:400+400*(7/8)
+        },
+        56:{
+            div:7,sound:400+400*(8/8)
+        }
+    }
+
+
+    var keys=document.querySelectorAll(".key");
+
+    var  audio=new AudioContext();
+    var ao;
+    document.onkeydown=function (e) {
+        ao=audio.createOscillator();
+        var as=audio.createAnalyser();
+        var gain=audio.createGain();
+        ao.connect(as);
+        ao.connect(gain);
+        gain.connect(audio.destination);
+        ao.frequency.setValueAtTime(hash[e.keyCode].sound,audio.currentTime);
+        ao.start(audio.currentTime);
+        keys[hash[e.keyCode].div].style.boxShadow="0 0 20px #000 inset"
+    }
+    document.onkeyup=function (e) {
+        ao.stop(audio.currentTime);
+        keys[hash[e.keyCode].div].style.boxShadow="none"
+    }
+}
